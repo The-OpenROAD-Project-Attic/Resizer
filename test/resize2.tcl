52ae0aa337be6f2284cbd4b70d7df45fa829dc4a
James Cherry <cherry@parallaxsw.com>
2019-06-03 08:47:39 -0700

    resize3

--- test/resize2.tcl
+++ test/resize2.tcl
@@ -4,6 +4,6 @@ read_liberty nlc18.lib
 read_lef nlc18.lef
 read_def reg1.def
 resize
-set def_file [make_result_file reg1_sized.def]
+set def_file [make_result_file resize2.def]
 write_def $def_file
 report_file $def_file
