30cfcce2aee9347d2c1dcebb0aede76e82e579e1
James Cherry <cherry@parallaxsw.com>
2019-06-09 23:37:34 -0700

    factor SteinerTree out of Resizer.cc

---
+++ SteinerTree.cc
@@ -0,0 +1,319 @@
+// Resizer, LEF/DEF gate resizer
+// Copyright (c) 2019, Parallax Software, Inc.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <fstream>
+#include <string>
+#include "Machine.hh"
+#include "Report.hh"
+#include "Error.hh"
+#include "Debug.hh"
+#include "SteinerTree.hh"
+
+namespace sta {
+
+using std::string;
+
+#ifdef FLUTE_2_2
+  // Wrapper so arglist is consistent across versions.
+  static Tree
+  flute1(int d, FLUTE_DTYPE x[], FLUTE_DTYPE y[], int acc, int mapping[])
+  {
+    return flute(d, x, y, acc, mapping);
+  }
+#endif
+
+static bool
+fileExists(const string &filename);
+
+bool
+readFluteInits(string dir)
+{
+  string flute_path1 = dir;
+  string flute_path2 = dir;
+  flute_path1 += "/etc/POWV9.dat";
+  flute_path2 += "/etc/PORT9.dat";
+  if (fileExists(flute_path1) && fileExists(flute_path2)) {
+    readLUT(flute_path1.c_str(), flute_path2.c_str());
+    return true;
+  }
+  else
+    return false;
+}
+
+// c++17 std::filesystem::exists
+static bool
+fileExists(const string &filename)
+{
+  std::ifstream stream(filename.c_str());
+  return stream.good();
+}
+
+////////////////////////////////////////////////////////////////
+
+SteinerPt SteinerTree::null_pt = -1;
+
+void
+SteinerTree::setTree(Tree tree,
+		     int pin_map[])
+{
+  tree_ = tree;
+
+  // Invert the steiner vertex to pin index map.
+  int pin_count = pinCount();
+  pin_map_.resize(pin_count);
+  for (int i = 0; i < pin_count; i++)
+    pin_map_[pin_map[i]] = i;
+  findSteinerPtAliases();
+}
+
+int
+SteinerTree::branchCount() const
+{
+  return tree_.deg * 2 - 2;
+}
+
+void
+SteinerTree::branch(int index,
+		    // Return values.
+		    DefPt &pt1,
+		    Pin *&pin1,
+		    int &steiner_pt1,
+		    DefPt &pt2,
+		    Pin *&pin2,
+		    int &steiner_pt2,
+		    int &wire_length)
+{
+  Branch &branch_pt1 = tree_.branch[index];
+  int index2 = branch_pt1.n;
+  Branch &branch_pt2 = tree_.branch[index2];
+  pt1 = DefPt(branch_pt1.x, branch_pt1.y);
+  if (index < pinCount()) {
+    pin1 = pin(index);
+    steiner_pt1 = 0;
+  }
+  else {
+    pin1 = nullptr;
+    steiner_pt1 = index;
+  }
+
+  pt2 = DefPt(branch_pt2.x, branch_pt2.y);
+  if (index2 < pinCount()) {
+    pin2 = pin(index2);
+    steiner_pt2 = 0;
+  }
+  else {
+    pin2 = nullptr;
+    steiner_pt2 = index2;
+  }
+
+  wire_length = abs(branch_pt1.x - branch_pt2.x)
+    + abs(branch_pt1.y - branch_pt2.y);
+}
+
+void
+SteinerTree::report(const Network *network)
+{
+  Report *report = network->report();
+  int branch_count = branchCount();
+  for (int i = 0; i < branch_count; i++) {
+    Branch &pt1 = tree_.branch[i];
+    int j = pt1.n;
+    Branch &pt2 = tree_.branch[j];
+    int wire_length = abs(pt1.x - pt2.x) + abs(pt1.y - pt2.y);
+    report->print(" %s (%d %d) - %s wire_length = %d",
+		  name(i, network),
+		  static_cast<int>(pt1.x),
+		  static_cast<int>(pt1.y),
+		  name(j, network),
+		  wire_length);
+    if (left_.size()) {
+      SteinerPt left = this->left(i);
+      SteinerPt right = this->right(i);
+      report->print(" left = %s right = %s",
+		    name(left, network),
+		    name(right, network));
+    }
+    report->print("\n");
+  }
+}
+
+void
+SteinerTree::findSteinerPtAliases()
+{
+  int pin_count = pinCount();
+  for(int i = 0; i < pin_count; i++) {
+    Branch &branch_pt1 = tree_.branch[i];
+    steiner_pt_pin_alias_map_[DefPt(branch_pt1.x, branch_pt1.y)] = pins_[pin_map_[i]];
+  }
+}
+
+Pin *
+SteinerTree::steinerPtAlias(SteinerPt pt)
+{
+  Branch &branch_pt = tree_.branch[pt];
+  return steiner_pt_pin_alias_map_[DefPt(branch_pt.x, branch_pt.y)];
+}
+
+const char *
+SteinerTree::name(SteinerPt pt,
+		  const Network *network)
+{
+  if (pt == SteinerTree::null_pt)
+    return "NULL";
+  else {
+    checkSteinerPt(pt);
+    const Pin *pin = this->pin(pt);
+    if (pin)
+      return network->pathName(pin);
+    else
+      return stringPrintTmp("S%d", pt);
+  }
+}
+
+Pin *
+SteinerTree::pin(SteinerPt pt) const
+{
+  checkSteinerPt(pt);
+  if (pt < pinCount())
+    return pins_[pin_map_[pt]];
+  else
+    return nullptr;
+}
+
+SteinerPt
+SteinerTree::drvrPt(const Network *network) const
+{
+  int pin_count = pinCount();
+  for (int i = 0; i < pin_count; i++) {
+    Pin *pin = this->pin(i);
+    if (network->isDriver(pin))
+      return i;
+  }
+  return SteinerTree::null_pt;
+}
+
+void
+SteinerTree::checkSteinerPt(SteinerPt pt) const
+{
+  if (pt < 0 || pt >= branchCount())
+    internalError("steiner point index out of range.");
+}
+
+bool
+SteinerTree::isLoad(SteinerPt pt,
+		    const Network *network)
+{
+  checkSteinerPt(pt);
+  Pin *pin = this->pin(pt);
+  return pin && network->isLoad(pin);
+}
+
+DefPt
+SteinerTree::location(SteinerPt pt) const
+{
+  checkSteinerPt(pt);
+  Branch &branch_pt = tree_.branch[pt];
+  return DefPt(branch_pt.x, branch_pt.y);
+}
+
+void
+SteinerTree::findLeftRights(const Network *network)
+{
+  int branch_count = branchCount();
+  left_.resize(branch_count, SteinerTree::null_pt);
+  right_.resize(branch_count, SteinerTree::null_pt);
+  int pin_count = pinCount();
+  SteinerPtSeq adj1(branch_count, SteinerTree::null_pt);
+  SteinerPtSeq adj2(branch_count, SteinerTree::null_pt);
+  for (int i = 0; i < branch_count; i++) {
+    Branch &branch_pt = tree_.branch[i];
+    SteinerPt j = branch_pt.n;
+    if (j != i) {
+      if (adj1[j] < 0)
+	adj1[j] = i;
+      else
+	adj2[j] = i;
+    }
+  }
+  SteinerPt root = drvrPt(network);
+  SteinerPt root_adj = adjacentPt(root);
+  // Kludge for when the steiner root vertex has non-branch.
+  if (root_adj == root)
+    root_adj = adj1[root];
+  left_[root] = root_adj;
+  findLeftRights(root, root_adj, adj1, adj2);
+}
+
+SteinerPt
+SteinerTree::adjacentPt(SteinerPt pt)
+{
+  return tree_.branch[pt].n;
+}
+
+void
+SteinerTree::findLeftRights(SteinerPt from,
+			    SteinerPt to,
+			    SteinerPtSeq &adj1,
+			    SteinerPtSeq &adj2)
+{
+  if (to >= pinCount()) {
+    SteinerPt adj;
+    adj = adjacentPt(to);
+    if (adj != to)
+      findLeftRights(from, to, adj, adj1, adj2);
+    adj = adj1[to];
+    if (adj >= 0)
+      findLeftRights(from, to, adj, adj1, adj2);
+    adj = adj2[to];
+    if (adj >= 0)
+      findLeftRights(from, to, adj, adj1, adj2);
+  }
+}
+
+void
+SteinerTree::findLeftRights(SteinerPt from,
+			    SteinerPt to,
+			    SteinerPt adj,
+			    SteinerPtSeq &adj1,
+			    SteinerPtSeq &adj2)
+{
+  if (adj != from) {
+    if (adj == to)
+      internalError("steiner left/right failed");
+    if (left_[to] < 0) {
+      left_[to] = adj;
+      findLeftRights(to, adj, adj1, adj2);
+    }
+    else if (right_[to] < 0) {
+      right_[to] = adj;
+      findLeftRights(to, adj, adj1, adj2);
+    }
+  }
+}
+
+SteinerPt
+SteinerTree::left(SteinerPt pt)
+{
+  return left_[pt];
+}
+
+SteinerPt
+SteinerTree::right(SteinerPt pt)
+{
+  return right_[pt];
+}
+
+};
---
+++ SteinerTree.hh
@@ -0,0 +1,144 @@
+// Resizer, LEF/DEF gate resizer
+// Copyright (c) 2019, Parallax Software, Inc.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef RESIZER_STEINER_TREE_H
+#define RESIZER_STEINER_TREE_H
+
+#include <string>
+#include "Hash.hh"
+#include "LefDefNetwork.hh"
+
+#define FLUTE_2_2
+
+#ifdef FLUTE_2_2
+  #include "flute.h"
+#endif
+
+#ifdef FLUTE_3_1
+  #define DTYPE sta::DefDbu
+  #include "flute.h"
+  typedef sta::DefDbu FluteDbu;
+  // Remove flute turds.
+  #undef max
+  #undef min
+  #undef abs
+#endif
+
+namespace sta {
+
+using std::string;
+
+#ifdef FLUTE_2_2
+  typedef DBU FluteDbu;
+  using Flute::Tree;
+  using Flute::Branch;
+  using Flute::readLUT;
+  using Flute::flute;
+  using Flute::printtree;
+#endif
+
+bool
+readFluteInits(string dir);
+
+class DefPtHash
+{
+public:
+  Hash operator()(const DefPt &pt) const
+  {
+    Hash hash = hash_init_value;
+    hashIncr(hash, pt.x());
+    hashIncr(hash, pt.y());
+    return hash;
+  }
+};
+
+class DefPtEqual
+{
+public:
+  bool operator()(const DefPt &pt1,
+		  const DefPt &pt2) const
+  {
+    return pt1.x() == pt2.x()
+      && pt1.y() == pt2.y();
+  }
+};
+
+typedef int SteinerPt;
+typedef Vector<SteinerPt> SteinerPtSeq;
+
+// Wrapper for Tree
+class SteinerTree
+{
+public:
+  SteinerTree() {}
+  PinSeq &pins() { return pins_; }
+  void setTree(Tree tree,
+	       int pin_map[]);
+  int pinCount() const { return pins_.size(); }
+  int branchCount() const;
+  void branch(int index,
+	      // Return values.
+	      DefPt &pt1,
+	      Pin *&pin1,
+	      int &steiner_pt1,
+	      DefPt &pt2,
+	      Pin *&pin2,
+	      int &steiner_pt2,
+	      int &wire_length);
+  void report(const Network *network);
+  // The steiner points can be in the same location as the pins.
+  void findSteinerPtAliases();
+  // Return a pin in the same location as the steiner pt if it exists.
+  Pin *steinerPtAlias(SteinerPt pt);
+  // Return the steiner pt connected to the driver pin.
+  SteinerPt drvrPt(const Network *network) const;
+
+  const char *name(SteinerPt pt,
+		   const Network *network);
+  SteinerPt adjacentPt(SteinerPt pt);
+  Pin *pin(SteinerPt pt) const;
+  bool isLoad(SteinerPt pt,
+	      const Network *network);
+  DefPt location(SteinerPt pt) const;
+  SteinerPt left(SteinerPt pt);
+  SteinerPt right(SteinerPt pt);
+  void findLeftRights(const Network *network);
+  static SteinerPt null_pt;
+
+protected:
+  void findLeftRights(SteinerPt from,
+		      SteinerPt to,
+		      SteinerPtSeq &adj1,
+		      SteinerPtSeq &adj2);
+  void findLeftRights(SteinerPt from,
+		      SteinerPt to,
+		      SteinerPt adj,
+		      SteinerPtSeq &adj1,
+		      SteinerPtSeq &adj2);
+  void checkSteinerPt(SteinerPt pt) const;
+
+  Tree tree_;
+  PinSeq pins_;
+  // steiner pt (tree vertex index) -> pin index
+  SteinerPtSeq pin_map_;
+  // location -> pin
+  UnorderedMap<DefPt, Pin*, DefPtHash, DefPtEqual> steiner_pt_pin_alias_map_;
+  SteinerPtSeq left_;
+  SteinerPtSeq right_;
+};
+
+} // namespace
+#endif
