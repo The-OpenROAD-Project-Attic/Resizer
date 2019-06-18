6c50ccc6652751a75cecbf66dfb162bc882bcccc
James Cherry <cherry@parallaxsw.com>
2019-06-18 15:55:24 -0700

    tests

--- test/resize2.tcl
+++ test/resize2.tcl
@@ -6,8 +6,9 @@ read_def reg1.def
 
 create_clock -name clk -period 1 {clk1 clk2 clk3}
 set_input_delay -clock clk 0 {in1 in2}
-# no placement, so add a load
+# no placement, so add loads
 set_load .2 u1z
+set_load .2 r1q
 
 report_checks
 resize -resize
